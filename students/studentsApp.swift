e1c140355831f2e37e3a3b79b5c5e2f2cd43ebdc
Perekalskyi Vladyslav <Perekalskyy.v@gmail.com>
2024-01-24 14:31:41 +0200

    Initial Commit

---
+++ students/studentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  studentsApp.swift
+//  students
+//
+//  Created by Vladyslav Perekalskyi on 24.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct studentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
